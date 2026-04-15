dd8550d31fe405885f7096e628b4afc34d3342e7
Bradi Manensi Mayorquin Mejia <bmayorquin@farinter.net>
2026-04-15 10:34:24 -0600

    Corrigiendo basePath para GitHub

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 1. Indica a Next.js que genere archivos HTML estáticos
   output: 'export', 
-  
-  // 2. Desactiva la optimización de imágenes nativa (porque requiere un servidor)
-  // Esto es necesario para que las fotos de los lentes se vean en GitHub Pages
   images: {
     unoptimized: true,
   },
-
-  // 3. OPCIONAL: Si tu URL de GitHub es algo como "usuario.github.io/nombre-repo"
-  // debes agregar el nombre del repositorio aquí:
-  // basePath: '/nombre-del-repo',
+  // Agrega esta línea para que reconozca la carpeta del repositorio
+  basePath: '/optica_honduras2026',
 };
 
 module.exports = nextConfig;
